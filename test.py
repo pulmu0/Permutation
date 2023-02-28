80f5b52562329381a99f7518cec5eb733e8419a6
pulmu0 <65453085+pulmu0@users.noreply.github.com>
2023-02-28 23:14:42 +0900

    python 형태로 변경 속도 증가

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
 import pdb
 
+def get_permutation(lists):
+    if len(lists) <= 1 :
+        return [lists]
+
+    result = []
+    for i,k in enumerate(lists):
+        head = k
+        tail = [x for j,x in enumerate(lists) if j != i ]
+        tails = get_permutation(tail)  #list of list
+        for j,tail in enumerate(tails):
+            k1 = [head,*tail] #tail.insert(0,head)
+            result.append(k1)
+
+    return result
+
 def callPermu(a):
+    # a is list
     count = len(a)
     if count <= 1 :
         return [a]  # [[a[0]]]
     
-    result = []
+    result = []  
     for i in range(count) :
         temp = []
         for j in range(count) : 
@@ -15,10 +31,6 @@ def callPermu(a):
                 temp.append(a[j])
         head = a[i]
         tail = callPermu(temp)
-        #pdb.set_trace()
-        #print(head)
-        #print('tail')
-        #print(tail)
 
         for j,ks in enumerate(tail) :
             temp1 = []
@@ -26,12 +38,12 @@ def callPermu(a):
             for k,k1 in enumerate(ks):
                 temp1.append(k1)
             result.append(temp1)
-        #temp.append(head)
     return result
+
 
 def test() :
     callPermu(temp)
-    #print(tail)
+
     for j,k1 in enumerate(tail) :  # in range(count - 1):
         ks = [] #head]
         ks.append(head)
@@ -41,15 +53,27 @@ def test() :
             
     return result
 
+def testList():
+    te = [1,3]
+    s = [x for i,x in enumerate(te) if x  ]
+    print(s)
+
 start = []  #1,2,3]
-for i in range(9):
+for i in range(4):
     start.append(i+1)
-    
-result = callPermu(start)
-print(result)
+
+print(start)
+result = get_permutation(start) # callPermu(start)
 print(len(result))
+print(result)
+#print(len(result))
+
+#testList()
+
 #for i,ks in enumerate(result):
 #    print(ks)
 #    pass
                       
         
+
+
